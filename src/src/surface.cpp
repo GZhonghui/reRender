aed9b6d0eb21a569a4ef9812f2bb91f651136cd0
GZhonghui <mail@gzher.com>
2020-07-14 00:12:21 +0800

    delete folder

--- src/src/surface.cpp
+++
@@ -1,9 +0,0 @@
-#include"surface.h"
-
-#include"object.h"
-#include"message.h"
-
-void Surface::fill_surface(const std::vector<Point> &particles,std::vector<Object *> &objects)
-{
-    Message::print(MessageType::MESSAGE,"Rebuild the surface done.");
-}
