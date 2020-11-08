4cc375c561b9fc57151782b76101fee9c55e56f9
GZhonghui <mail@gzher.com>
2020-11-08 17:49:14 +0800

    fix output bug

--- src/basic/message.cpp
+++ src/basic/message.cpp
@@ -6,13 +6,14 @@ void Message::print(MessageType message_type,const char *message,
     bool enable_file,bool enable_time)
 {
     char log_file_path[message_size];
-    time_t now=time(nullptr);
+    time_t now_time=time(nullptr);
     FILE *log_file(nullptr);
-    tm *ltm=localtime(&now);
-    ltm->tm_mon+=1;
+    tm *local_time=localtime(&now_time);
+    local_time->tm_mon+=1;
 
-    sprintf(log_file_path,"./logs/%04d-%02d-%02d.txt",1900+ltm->tm_year,
-        ltm->tm_mon,ltm->tm_mday);
+    sprintf(log_file_path,"./logs/%04d-%02d-%02d.txt",
+        1900+local_time->tm_year,local_time->tm_mon,local_time->tm_mday);
+
     if(enable_file)
     {
         log_file=fopen(log_file_path,"a");
@@ -30,8 +31,9 @@ void Message::print(MessageType message_type,const char *message,
             {
                 fprintf(log_file,"[MESSAGE]");
             }
-            break;
         }
+        break;
+
         case MessageType::ERROR:
         {
             printf("\033[31m[ ERROR ]\033[0m");
@@ -41,6 +43,7 @@ void Message::print(MessageType message_type,const char *message,
             }
         }
         break;
+
         case MessageType::WARNING:
         {
             printf("\033[33m[WARNING]\033[0m");
@@ -53,12 +56,14 @@ void Message::print(MessageType message_type,const char *message,
     }
     if(enable_time)
     {
-        printf(" \033[34m[%04d-%02d-%02d %02d:%02d:%02d]\033[0m",1900+ltm->tm_year,
-            ltm->tm_mon,ltm->tm_mday,ltm->tm_hour,ltm->tm_min,ltm->tm_sec);
+        printf(" \033[34m[%04d-%02d-%02d %02d:%02d:%02d]\033[0m",
+            1900+local_time->tm_year,local_time->tm_mon,local_time->tm_mday,
+            local_time->tm_hour,local_time->tm_min,local_time->tm_sec);
         if(enable_file&&log_file)
         {
-            fprintf(log_file," [%04d-%02d-%02d %02d:%02d:%02d]",1900+ltm->tm_year,
-                ltm->tm_mon,ltm->tm_mday,ltm->tm_hour,ltm->tm_min,ltm->tm_sec);
+            fprintf(log_file," [%04d-%02d-%02d %02d:%02d:%02d]",
+                1900+local_time->tm_year,local_time->tm_mon,local_time->tm_mday,
+                local_time->tm_hour,local_time->tm_min,local_time->tm_sec);
         }
     }
     printf(" %s\n",message);
@@ -76,32 +81,33 @@ void Message::print_once(unsigned int id,const char *message)
     {
         printed.insert(id);
 
-        time_t now=time(nullptr);
-        tm *ltm=localtime(&now);
-        ltm->tm_mon+=1;
+        time_t now_time=time(nullptr);
+        tm *local_time=localtime(&now_time);
+        local_time->tm_mon+=1;
 
         printf("\033[33m[ DEBUG ]\033[0m");
-        printf(" \033[34m[%04d-%02d-%02d %02d:%02d:%02d]\033[0m",1900+ltm->tm_year,
-            ltm->tm_mon,ltm->tm_mday,ltm->tm_hour,ltm->tm_min,ltm->tm_sec);
+        printf(" \033[34m[%04d-%02d-%02d %02d:%02d:%02d]\033[0m",
+            1900+local_time->tm_year,local_time->tm_mon,local_time->tm_mday,
+            local_time->tm_hour,local_time->tm_min,local_time->tm_sec);
         printf(" %s\n",message);
+        fflush(stdout);
     }
-    fflush(stdout);
 }
 
-void Message::print_bar(int now,bool last,int width)
+void Message::print_bar(int now_time,bool last,int width)
 {
-    now=std::min(100,std::max(0,now));
+    now_time=std::min(100,std::max(0,now_time));
     width=std::min(100,std::max(20,width));
 
     char message[message_size];
     int real_width=width-12;
-    int blocks=std::max(1,(int)(now*width/100.0));
+    int blocks=std::max(1,(int)(now_time*width/100.0));
     for(int i=0;i<=width;i+=1)
     {
         message[i]=(i==width?0:(i==blocks-1?'>':(i>=blocks?' ':'=')));
     }
 
-    printf("\r\033[32m[ #%03d%% ]\033[0m [%s]",now,message);
+    printf("\r\033[32m[ #%03d%% ]\033[0m [%s]",now_time,message);
     if(last) puts("");
     fflush(stdout);
 }
